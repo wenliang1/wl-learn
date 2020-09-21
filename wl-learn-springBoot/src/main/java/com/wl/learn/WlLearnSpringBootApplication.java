44bd7d533527f25d7dadb20bed81e8c3a7225b98
wenliang <547069625@qq.com>
2020-09-21 10:46:26 +0800

    项目初始化

---
+++ wl-learn-springBoot/src/main/java/com/wl/learn/WlLearnSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.wl.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WlLearnSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WlLearnSpringBootApplication.class, args);
+	}
+
+}
---
+++ wl-learn-springBoot/src/test/java/com/wl/learn/WlLearnSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wl.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WlLearnSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
